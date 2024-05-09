1de6a799dbb507fbb638be8c9e08a11c95908c95
admin <admin@admindeMac-mini.local>
2024-05-09 13:53:09 +0800

    fix

--- Stock-8003/src/main/java/org/example/mq/StockReceiver.java
+++ Stock-8003/src/main/java/org/example/mq/StockReceiver.java
@@ -19,6 +19,10 @@ import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.io.IOException;
 import java.util.Date;
 
@@ -29,16 +33,9 @@ import static org.example.config.RabbitMqConfig.*;
 @Slf4j
 @RabbitListener(queues = Stock_Release_Stock_Queue_Name)
 public class StockReceiver {
-    @Resource
-    private StockOnDoLogService stockOnDoLogService;
-    @Resource
-    private StockClientService stockClientService;
 
     @Resource
-    private StockService stockService;
-
-    @Resource
-    private RabbitTemplate rabbitTemplate;
+    private StockRollbackCheck stockRollbackCheck;
 
     /**
      * 檢查
@@ -58,115 +55,7 @@ public class StockReceiver {
     @RabbitHandler
     private void stockReceiver(CheckStockMq checkStock , Channel channel , Message msg) throws IOException {
         log.info("Received stock check: {}", checkStock);
-
-        Long stockUndoLogId = checkStock.getStock_undo_log_id();
-        Long stockId = checkStock.getStock_id();
-        Long orderId = checkStock.getOrder_id();
-
-        StockOnDoLog stockOnDoLog = findStockOnDoLog(stockUndoLogId, stockId, orderId);
-
-        //操作日誌有 在創建庫存時沒有回滾
-        if (stockOnDoLog != null) {
-            handleStockOperation(stockOnDoLog, channel, msg);
-        } else {
-            //庫存操作沒有 在扣庫存時就本地回滾了 無需處理
-            log.error("StockOnDoLog not found.");
-            channel.basicAck(msg.getMessageProperties().getDeliveryTag(), false);
-        }
-    }
-
-    /**
-     * 匹配訂單日誌
-     */
-    private StockOnDoLog findStockOnDoLog(Long stockUndoLogId, Long stockId, Long orderId) {
-        if (stockUndoLogId != null) {
-            return stockOnDoLogService.getStockOnDoLogDaoById(stockUndoLogId);
-        } else if (stockId != null && orderId != null) {
-            return stockOnDoLogService.getStockOnDoLogDaoByStockIdAndOrderId(stockId, orderId);
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * 確認訂單狀態
-     */
-    private void handleStockOperation(StockOnDoLog stockOnDoLog, Channel channel, Message msg) throws IOException {
-        //判斷是否要重新排隊
-        boolean reQueue = true;
-        try {
-            //1.訂單創建時打完扣StockAPI後是否有回滾 訂單回滾庫存需要補償
-            Order order = RepOrder(stockClientService.getOrderById(stockOnDoLog.getOrder_id()));
-            if (order != null) {
-                switch (order.getStatus()) {
-                    //定單狀態 Fail = 0, Success=1  ,CreateIng = 2 , PayIng = 3
-                    case 1:
-                        //訂單已正常支付無需回滾 更新庫存日誌
-                        updateStockLog(stockOnDoLog, 1);
-                        reQueue = false;
-                        channel.basicAck(msg.getMessageProperties().getDeliveryTag(), false);
-                        break;
-                    case -1:
-                        //訂單支付失敗需回滾 更新庫存日誌
-                        rollbackStock(stockOnDoLog);
-                        reQueue = false;
-                        channel.basicAck(msg.getMessageProperties().getDeliveryTag(), false);
-                        break;
-                }
-                //查無訂單 訂單回滾 庫存也需要回滾   操作: 扣除 操作狀態: 等待
-            } else if (OperationTypeEnum.Decrease.name.equals(stockOnDoLog.getOperation_type())
-                    && stockOnDoLog.getRollback_status() == 0) {
-                rollbackStock(stockOnDoLog);
-                reQueue = false;
-                channel.basicAck(msg.getMessageProperties().getDeliveryTag(), false);
-            }
-        } catch (OKHttpException | OrderServerErrorException e) {
-            //打訂單服務時失敗 重新排隊
-            log.error(e.getMessage());
-        }
-
-        //其他未知狀況放回隊列
-        if (reQueue) {
-            reQueueMessage(stockOnDoLog, channel, msg);
-        }
-    }
-
-    /**
-     * 更新庫存日誌
-     */
-    private void updateStockLog(StockOnDoLog stockOnDoLog, int status) {
-        StockOnDoLog newStockLog = StockOnDoLog.builder()
-                .id(stockOnDoLog.getId())
-                .status(status)
-                .build();
-        stockOnDoLogService.updateStockOnDoLog(newStockLog);
-    }
-
-    /**
-     * 回滾庫存
-     */
-    private void rollbackStock(StockOnDoLog stockOnDoLog) {
-        stockService.increaseQuantity(stockOnDoLog.getStock_id(), stockOnDoLog.getQuantity());
-        StockOnDoLog newStockLog = StockOnDoLog.builder()
-                .id(stockOnDoLog.getId())
-                .status(-1)
-                .rollback_status(RollbackStatusEnum.IsRollback.code)
-                .rollback_time(new Date())
-                .build();
-        stockOnDoLogService.updateStockOnDoLog(newStockLog);
-    }
-
-    /**
-     * 重新放回隊列 等待下次檢查
-     */
-    private void reQueueMessage(StockOnDoLog stockOnDoLog, Channel channel, Message msg) throws IOException {
-        CheckStockMq checkStockMq = CheckStockMq.builder()
-                .stock_undo_log_id(stockOnDoLog.getId())
-                .stock_id(stockOnDoLog.getStock_id())
-                .order_id(stockOnDoLog.getOrder_id())
-                .build();
-        rabbitTemplate.convertAndSend(Stock_Event_Exchange, Stock_Locked_Key, checkStockMq);
-        channel.basicAck(msg.getMessageProperties().getDeliveryTag(), false);
+        stockRollbackCheck.stockCheck(checkStock,channel,msg);
     }
 
 }
